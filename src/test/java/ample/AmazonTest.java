c022459238c7e4ad2ef57b0c36861c66b1e34a5d
sasikala-26 <sasi10397@gmail.com>
2025-04-17 20:30:44 +0530

    push

---
+++ src/test/java/ample/AmazonTest.java
@@ -0,0 +1,21 @@
+package ample;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+
+public class AmazonTest {
+@Test
+
+public void AmazonTest() {
+	
+	WebDriver driver=new ChromeDriver();
+	driver.get("https://www.facebook.com/");
+	  WebElement ele=driver.findElement(By.name("email"));
+	  ele.sendKeys("abc");
+	  System.out.println(ele.getAttribute("value"));
+}
+}
